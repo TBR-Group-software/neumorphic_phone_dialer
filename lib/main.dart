cdae95a0a071d17d3bdab0bbf2c67a6e617adc72
VadimBliz <vadimbliz98@gmail.com>
2022-09-20 16:12:34 +0400

    add app theme switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:neumorphic_phone_dialer/backbone/dependency_injection.dart'
+    as di;
+import 'package:neumorphic_phone_dialer/presentation/navigation/router.gr.dart';
+import 'package:neumorphic_phone_dialer/presentation/theme/base.dart';
+import 'package:neumorphic_phone_dialer/presentation/theme/dark.dart';
+import 'package:neumorphic_phone_dialer/presentation/theme/lite.dart';
 
 void main() {
-  runApp(const MyApp());
+  di.init();
+
+  runApp(
+    const DialerApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class DialerApp extends StatefulWidget {
+  const DialerApp({Key? key}) : super(key: key);
+
+  @override
+  State<DialerApp> createState() => _DialerAppState();
+}
+
+class _DialerAppState extends State<DialerApp> {
+  late final AppRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _router = AppRouter();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Neumorphic Phone Dialer',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
+      routerDelegate: _router.delegate(),
+      routeInformationParser: _router.defaultRouteParser(),
+      builder: (BuildContext context, Widget? child) {
+        return AppTheme(
+          data: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? darkTheme
+              : liteTheme,
+          child: child ?? const SizedBox(),
+        );
+      },
     );
   }
 }
